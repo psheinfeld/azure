b50f46c614f931b107d355a2fe80a6295160d36d
psheinfeld <pavel.sheinfeld@gmail.com>
2025-01-28 12:55:38 +0200

    state to remote

--- ALB/public-alb-chain.tf
+++ ALB/public-alb-chain.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = var.state_resource_group_name
+    storage_account_name  = var.state_storage_account_name
+    container_name        = var.state_container_name
+    key                   = var.state_key
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
--- ALB/variables.tf
+++ ALB/variables.tf
@@ -6,5 +6,26 @@ variable "subscription_id" {
 variable "ssh_public_key" {
   description = "SSH public key"
   type        = string
-  
+}
+
+variable "state_subscription_id" {
+  description = "TF state subscription id"
+  type        = string
+}
+variable "state_resource_group_name" {
+  description = "TF state resource group name"
+  type        = string
+}
+
+variable "state_storage_account_name" {
+  description = "TF state storage account name"
+  type        = string
+}
+variable "state_container_name" {    
+  description = "TF state container name"
+  type        = string 
+}
+variable "state_key" {
+  description = "TF state key"
+  type        = string
 }
